36c5b8e8a58f29153a33c5bc7915b48fa42555a2
rossygavin <rossygavin@gmail.com>
2023-11-16 23:37:27 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit
 
-streamlit.header(🥣'Breakfast Menu')
-streamlit.text(🥗'Omega 3 & Blueberry Oatmeal')
-streamlit.text(🐔'Kale, Spinach & Rocket Smoothie')
-streamlit.text(🥑🍞'Hard-Boiled Free-Range Egg')
+streamlit.header('🥣Breakfast Menu')
+streamlit.text('🥗Omega 3 & Blueberry Oatmeal')
+streamlit.text('🐔Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥑🍞'Hard-Boiled Free-Range Egg')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
