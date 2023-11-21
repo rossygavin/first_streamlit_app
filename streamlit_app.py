eff55cbe6cde113813941e16c707b44bcb433e35
rossygavin <rossygavin@gmail.com>
2023-11-21 16:43:10 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,12 @@
 import streamlit
 import pandas
 
-streamlit.header('🥣Breakfast Menu')
-streamlit.text('🥗Omega 3 & Blueberry Oatmeal')
-streamlit.text('🐔Kale, Spinach & Rocket Smoothie')
-streamlit.text('🥑🍞Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.header('Build Your Own Fruit Smoothie')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
