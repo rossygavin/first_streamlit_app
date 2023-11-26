1932a37362b8aa331062818014d06a665ee12797
rossygavin <rossygavin@gmail.com>
2023-11-26 23:43:43 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ def get_fruit_load_list():
 if streamlit.button('Get fruit load list'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
-  streamlit.dataframe(my_data_row)
+  streamlit.dataframe(my_data_rows)
 
 def insert_row_into_snowflake(new_fruit):
   with my_cnx.cursor as my_cur:
